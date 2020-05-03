b197867f8fd2a1312c9eac1c715e95013d1b5428
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-03 08:24:03 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   
   def change
-      add_column :grade TEXTS
+      add_column :grade TEXT
       add_column :birthdate TEXT
   end 
   
