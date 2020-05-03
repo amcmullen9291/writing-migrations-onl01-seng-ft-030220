1673c5050c6e22514591737f214796f216c3f520
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-03 08:36:07 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,10 +1,11 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   
-create_table :artists do |t|
+def change
+    add_column :artists do |t|
       t.string :name
       t.string :genre
       t.integer :age
       t.string :hometown
     end
-  
+
 end
