4ba72a6ba99026b463305d60d694ee66322d528a
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-03 08:32:55 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,9 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   
   def change
-    change_column :students do |t|
-      t.datetime :birthdate
-    end
+    change_column :students, :birthdate, :datetime
   end
 
 end
